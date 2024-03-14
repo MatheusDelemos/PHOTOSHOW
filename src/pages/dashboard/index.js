1c30abc1636a0a66e6d60c515dd1df598a887db9
Matheus de Lemos <matheus.delemos2507@gmail.com>
2024-03-13 22:53:41 -0300

    ajeitando arquivos

---
+++ src/pages/dashboard/index.js
@@ -0,0 +1,14 @@
+import { Text, View } from 'react-native'
+import React, { Component } from 'react'
+
+const Dashboard = ()=>{
+ return (
+      <View>
+        <Text>index</Text>
+      </View>
+    )
+  
+}
+
+export default Dashboard
+
